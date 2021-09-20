dab6733da08f71ece8fceb3dfdab398822060f22
Karen Melo <karen_melo@live.com>
2021-09-19 22:52:39 -0300

    feat: implementação de token, modificação da forma de usar claims.

---
+++ src/DevIO.Api/Configuration/TokenValidationParameters.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace DevIO.Api.Configuration
+{
+    internal class TokenValidationParameters<T1, T2> : TokenValidationParameters
+    {
+    }
+}
--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,10 +1,13 @@
 ﻿using DevIO.Api.DTO;
+using DevIO.Api.Extensions;
 using DevIO.Business.Intefaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
@@ -14,20 +17,23 @@ namespace DevIO.Api.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
         public AuthController(INotificador notificador,
                               SignInManager<IdentityUser> signInManager,
-                              UserManager<IdentityUser> userManager
-                              ):base (notificador)
+                              UserManager<IdentityUser> userManager,
+                              IOptions<AppSettings> appSettings
+                              ) : base(notificador)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("nova-conta")]
         public async Task<ActionResult> Registrar(RegisterUserDTO registerUser)
         {
-            if(!ModelState.IsValid) return CustomResponse(ModelState);
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
             var user = new IdentityUser
             {
@@ -41,10 +47,10 @@ namespace DevIO.Api.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return CustomResponse(registerUser);
+                return CustomResponse(GerarJwt(registerUser.Email));
             }
 
-            foreach(var error in result.Errors)
+            foreach (var error in result.Errors)
             {
                 NotificarErro(error.Description);
             }
@@ -55,13 +61,13 @@ namespace DevIO.Api.Controllers
         [HttpPost("entrar")]
         public async Task<ActionResult> Login(LoginUserDTO loginUser)
         {
-            if(!ModelState.IsValid) return CustomResponse(ModelState);
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
             var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
 
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
-                return CustomResponse(loginUser);
+                return CustomResponse(GerarJwt(loginUser.Email));
             }
             if (result.IsLockedOut)
             {
@@ -73,5 +79,21 @@ namespace DevIO.Api.Controllers
             return CustomResponse(loginUser);
         }
 
+        private string GerarJwt(string email)
+        {
+            var tokenHadler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var token = tokenHadler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            });
+
+            var encodedToken = tokenHadler.WriteToken(token);
+
+            return encodedToken;
+        }
     }
 }
